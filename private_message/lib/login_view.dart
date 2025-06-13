58160ec2cdf64b2bee06ed02be54da004891fa13
Jholian Mannnuel <jholianmanuel10@gmail.com>
2025-06-13 18:25:03 -0500

    trabajando en mi vista de login

--- private_message/lib/login_view.dart
+++ private_message/lib/login_view.dart
@@ -10,6 +10,169 @@ class LoginView extends StatefulWidget {
 class _LoginViewState extends State<LoginView> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    final size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // campo usuario
+            SizedBox(
+              width: size.width * 0.8,
+              child: TextField(
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                  hintText: 'Correo electrónico o número de teléfono',
+                  hintStyle: const TextStyle(
+                    color: Color.fromARGB(255, 0, 0, 0),
+                    fontWeight: FontWeight.w700,
+                  ),
+                  prefixIcon: const Icon(
+                    Icons.person,
+                    color: Color.fromARGB(255, 0, 0, 0),
+                  ),
+                  contentPadding: const EdgeInsets.symmetric(
+                    vertical: 18,
+                    horizontal: 16,
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15),
+                    borderSide: const BorderSide(
+                      color: Color.fromARGB(255, 0, 0, 0),
+                    ),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15),
+                    borderSide: const BorderSide(
+                      color: Color.fromARGB(255, 76, 0, 255),
+                    ),
+                  ),
+                ),
+                onChanged: (value) {},
+              ),
+            ),
+
+            const SizedBox(height: 12),
+
+            // campo contraseña
+            SizedBox(
+              width: size.width * 0.8,
+              child: TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  hintText: 'Contraseña',
+                  hintStyle: const TextStyle(
+                    color: Color.fromARGB(255, 0, 0, 0),
+                    fontWeight: FontWeight.w700,
+                  ),
+                  prefixIcon: const Icon(
+                    Icons.lock_outline,
+                    color: Color.fromARGB(255, 0, 0, 0),
+                  ),
+                  contentPadding: const EdgeInsets.symmetric(
+                    vertical: 18,
+                    horizontal: 16,
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15),
+                    borderSide: const BorderSide(
+                      color: Color.fromARGB(255, 0, 0, 0),
+                    ),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15),
+                    borderSide: const BorderSide(
+                      color: Color.fromARGB(255, 0, 94, 255),
+                    ),
+                  ),
+                ),
+                onChanged: (value) {},
+              ),
+            ),
+
+            const SizedBox(height: 20),
+
+            // botón iniciar sesion
+            ElevatedButton(
+              onPressed: () {
+                // botón de verificación de credenciales
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: const Color.fromARGB(
+                  221,
+                  255,
+                  255,
+                  255,
+                ), // Sin fondo
+                foregroundColor: const Color.fromARGB(
+                  255,
+                  0,
+                  0,
+                  0,
+                ), // Color del texto
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 150,
+                  vertical: 16,
+                ),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30),
+                  side: const BorderSide(
+                    // Borde azul
+                    color: Color.fromARGB(255, 0, 0, 0),
+                    width: 2,
+                  ),
+                ),
+              ),
+              child: const Text(
+                'Iniciar Sesión',
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold, // Para que resalte más
+                ),
+              ),
+            ),
+
+            const SizedBox(height: 12),
+
+            //texto ¿Olvidaste tu contraseña?
+            GestureDetector(
+              onTap: () {
+                //logica de recuperacion de contraseña
+              },
+              child: const Text(
+                '¿Olvidaste tu contraseña?',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            ),
+
+            const SizedBox(height: 100),
+
+            ElevatedButton(
+              onPressed: () {
+                //te mada a crear tu tuenta
+              },
+              style: ElevatedButton.styleFrom(
+                foregroundColor: const Color.fromARGB(255, 0, 0, 0),
+                minimumSize: const Size(400, 60),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30),
+                  side: const BorderSide(
+                    color: Color.fromARGB(255, 0, 0, 0),
+                    width: 2,
+                  ),
+                ),
+              ),
+              child: const Text(
+                'Crear cuenta',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
--- private_message/lib/main.dart
+++ private_message/lib/main.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:private_message/login_view.dart';
 
+void main() {
+  runApp(MyApp());
+}
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: LoginView());
+    return MaterialApp(debugShowCheckedModeBanner: false, home: LoginView());
   }
 }
