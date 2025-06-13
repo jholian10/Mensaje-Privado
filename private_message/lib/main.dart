e95605bfc0d3287e6ee47e68cf8c8ea040ad5c0c
Jholian Mannnuel <jholianmanuel10@gmail.com>
2025-06-13 15:42:30 -0500

    Empiezo a diseñar mi vista de login

--- private_message/lib/main.dart
+++ private_message/lib/main.dart
@@ -1,3 +1,11 @@
-void main() {
-  print('hola mundo');
+import 'package:flutter/material.dart';
+import 'package:private_message/login_view.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: LoginView());
+  }
 }
