2b2ed5d3cccb4be5f24536705ea5b6534c545acc
Jholian Mannnuel <jholianmanuel10@gmail.com>
2025-06-17 17:18:17 -0500

    termino mi vista de failuri
    vista sencilla bordes negro y un botton null para mostrar que ocurrió un error

--- private_message/lib/error_view.dart
+++ private_message/lib/error_view.dart
@@ -4,12 +4,48 @@ class ErrorView extends StatefulWidget {
   const ErrorView({super.key});
 
   @override
-  State<ErrorView> createState() => _ErrorViewState();
+  ErrorViews createState() => ErrorViews();
 }
 
-class _ErrorViewState extends State<ErrorView> {
+class ErrorViews extends State<ErrorView> with SingleTickerProviderStateMixin {
+  bool _showButton = true;
+
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(seconds: 3), () {
+      setState(() {
+        _showButton = false;
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Container(
+      margin: const EdgeInsets.all(4),
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.black, width: 2),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: Center(
+          child: AnimatedOpacity(
+            opacity: _showButton ? 1.0 : 0.0,
+            duration: const Duration(milliseconds: 600),
+            child: ElevatedButton(
+              onPressed: null,
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.red,
+                disabledBackgroundColor: Colors.red,
+                foregroundColor: Colors.white,
+                disabledForegroundColor: Colors.white,
+              ),
+              child: const Text('Loading Failure'),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
