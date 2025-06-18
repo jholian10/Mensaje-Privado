d06090343acba9a53c3a6b7b5821ee27fadaf3c9
jholian manuel <jholianmanuel10@gmail.com>
2025-06-18 10:37:05 -0500

    empiezo a trabajar en la vista de configuracion de mi app

---
+++ private_message/lib/my_views/setin_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SetinView extends StatefulWidget {
+  const SetinView({super.key});
+
+  @override
+  State<SetinView> createState() => _SetinViewState();
+}
+
+class _SetinViewState extends State<SetinView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
